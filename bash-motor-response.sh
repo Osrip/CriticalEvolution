afa0009961118b9857339408ab011adc38017b28
Jan <jan.prosi@hotmail.com>
2020-04-06 15:01:40 +0200

    Added Sina's new motor response functions

--- bash-motor-response.sh
+++ bash-motor-response.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-parallel --bar 'python3 compute-motor-response {1}' ::: $(seq 100)
+echo "Choose a simulation: "
+read sim
+echo "Define movement function (Move/MoveOld/MoveVelMot): "
+read moveCase
+echo "Starting Motor Response Calculations"
+
+parallel --bar --eta -j4 "python3 compute-motor-response $sim $moveCase {1} {2} {3}" ::: \
+$(seq 0 99) ::: \
+$(seq 0 2) ::: \
+0 500 2000 7999
 
